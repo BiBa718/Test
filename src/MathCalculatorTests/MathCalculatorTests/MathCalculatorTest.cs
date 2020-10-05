345c295b9b74bd5adb4d1cfaad5a6106e7da202d
BiBa <crotow.tony@gmail.com>
2020-10-05 14:11:27 +0300

    rename files

---
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using MathCalculator;
+
+namespace MathCalculatorTests
+{
+    public class MathCalculatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            const int five = 5;
+            const int three = 3;
+            MathCalculator calculator = new MathCalculator();
+        }
+    }
+}
