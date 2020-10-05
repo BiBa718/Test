84e2de88cc20c57ec22d96131b98402489a15bcb
BiBa <crotow.tony@gmail.com>
2020-10-05 17:24:37 +0300

    save changes

--- src/MathCalculatorTests/MathCalculatorTest.cs
+++ src/MathCalculatorTests/MathCalculatorTest.cs
@@ -5,13 +5,8 @@ namespace MathCalculatorTests
 {
     public class MathCalculatorTest
     {
-        private const int Ten = 10; 
-        private const int Six = 6;
         private const int Five = 5;
-        private const int Four = 4;
-        private const int Three = 3;
-        private const int Two = 2;
-        private const int Zero = 0;       
+        private const int Three = 3;     
 
         [Fact]
         public void WhenSumFiveAndThreeThenGotEight()
