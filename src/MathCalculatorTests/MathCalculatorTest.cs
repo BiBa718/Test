382a4ed5b7f65c715fb54c068e6b7ae8820e8648
BiBa <crotow.tony@gmail.com>
2020-10-05 15:38:30 +0300

    rename file

---
+++ src/MathCalculatorTests/MathCalculatorTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using MathCalculator;
+
+namespace MathCalculatorTests
+{
+    public class MathCalculatorTests
+    {
+        private const int Ten = 10; 
+        private const int Six = 6;
+        private const int Five = 5;
+        private const int Four = 4;
+        private const int Three = 3;
+        private const int Two = 2;
+        private const int Zero = 0;
+
+        [Fact]
+        public void WhenSumFiveAndThreeThenGotEight()
+        {
+            MathCalculator calculator = new MathCalculator();
+
+            
+        }
+    }
+}
